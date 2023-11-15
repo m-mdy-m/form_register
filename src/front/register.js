5052fabacfcba51fafadeb4ec02b8fd853850f38
mahdi <mahdimamashli1383@gmail.com>
2023-11-16 01:45:34 +0330

    add emoji and change style password

--- src/front/register.js
+++ src/front/register.js
@@ -1,6 +1,9 @@
 const labels = document.querySelectorAll('.form-input label')
-
+const inputs = document.querySelectorAll('.form-input input')
 
 labels.forEach(label =>{
     label.innerHTML = label.innerText.split('').map((letter,idx)=>`<span style="transition-delay:${idx * 50}ms">${letter}</span>`).join(' ')
+})
+inputs.forEach(input =>{
+    
 })
