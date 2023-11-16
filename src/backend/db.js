43ef9d44917186a5eef423e4c0a8dc5027860aef
mahdi <mahdimamashli1383@gmail.com>
2023-11-17 02:58:26 +0330

    delete backend project

--- src/backend/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-
-const database = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "dashboard_admin",
-});
-module.exports = database;
