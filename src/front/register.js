ec1500e581164e131a73ca947c52c7b406ae5cd5
mahdi <mahdimamashli1383@gmail.com>
2023-11-17 00:15:56 +0330

    add alert form

--- src/front/register.js
+++ src/front/register.js
@@ -1,15 +1,62 @@
 const labels = document.querySelectorAll(".form-input label");
 const showPassword = document.querySelector("#showPassword");
-let password = document.querySelector("#password");
 const wrapperName = document.getElementById("wrapper-name");
 const wrapperUsername = document.getElementById("wrapper-username");
 const wrapperEmail = document.getElementById("wrapper-email");
 const bgImg = document.getElementById("bgImg");
 let form = document.querySelector("form");
-let pass_length = password.value.length;
 let signUp = document.querySelector("#sign-Up");
 let signIn = document.querySelector("#sign-In");
 let register_login = document.getElementById("register_login");
+const first_name = document.getElementById("first_name");
+const userName = document.getElementById("user-name");
+const email = document.getElementById("email");
+let password = document.querySelector("#password");
+
+
+function checkPassword(password) {
+  // Check if the password is at least 8 characters long
+  if (password.length < 8) {
+    // Show an alert
+    alert("The password must be at least 8 characters long.");
+    return false;
+  }
+
+  // Check if the password contains at least one uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    // Show an alert
+    alert("The password must contain at least one uppercase letter.");
+    return false;
+  }
+
+  // Check if the password contains at least one lowercase letter
+  if (!/[a-z]/.test(password)) {
+    // Show an alert
+    alert("The password must contain at least one lowercase letter.");
+    return false;
+  }
+
+  // Check if the password contains at least one number
+  if (!/[0-9]/.test(password)) {
+    // Show an alert
+    alert("The password must contain at least one number.");
+    return false;
+  }
+
+  // Check if the password contains at least one special character
+  if (!/[!@#$%^&*()_+-={}[\]\\;':",.<>?]/.test(password)) {
+    // Show an alert
+    alert("The password must contain at least one special character.");
+    return false;
+  }
+
+  // The password is valid
+  return true;
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkPassword(password.value)
+});
 
 // =====  Creative text label  ==== ///
 
@@ -52,18 +99,18 @@ signUp.addEventListener("click", () => {
   form.style.cssText = "grid-template-rows: repeat(5, minmax(0, 1fr));";
 });
 
-
-
 // =====  change background ==== ///
 var i = 0;
 function changeBg() {
   i++;
   if (i > 4) {
-    bgImg.style.transition = "background 3s ease-in-out";
+    bgImg.style.opacity = "0";
+    bgImg.style.transition = "all 3s ease-in-out";
     i = 1;
   }
   bgImg.style.background = "url('../../image/bg/bg_" + i + ".jpg')";
-  bgImg.style.transition = "background 3s ease-in-out";
+  bgImg.style.transition = "all 3s ease-in-out";
+  bgImg.style.opacity = "1";
   setTimeout(changeBg, 5000);
 }
 changeBg();
